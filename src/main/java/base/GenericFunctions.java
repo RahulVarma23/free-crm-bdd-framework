23e3a23e3895f037351bee23fc6a22019774f0d7
rahulvarma23 <rahulvarma411@gmail.com>
2023-04-30 13:31:44 +0530

    refactor imports

--- src/main/java/base/GenericFunctions.java
+++ src/main/java/base/GenericFunctions.java
@@ -1,14 +1,8 @@
 package base;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
